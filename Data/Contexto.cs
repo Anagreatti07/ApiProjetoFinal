e75628eff59d79d2cf4caa23e416df88d47de3f1
Anagreatti07 <ana.greatti10@gmail.com>
2024-06-05 14:37:38 -0300

    finalizado

--- Data/Contexto.cs
+++ Data/Contexto.cs
@@ -12,9 +12,17 @@ namespace Api.Data
 
         public DbSet<UsersModel> User { get; set; }
 
+        public DbSet<UsuariosModel> Usuario { get; set; }
+
+        public DbSet<ObservacoesModel> Observacoes { get; set; }
+        public DbSet<ObjetosModel> Objeto { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UsersMap());
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new ObservacoesMap());
+            modelBuilder.ApplyConfiguration(new ObjetoMap());
             base.OnModelCreating(modelBuilder);
         }
 
---
+++ Models/ObservacoesModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Models
+{
+    public class ObservacoesModel
+    {
+        public int ObservacaoId { get; set; }
+        public string ObservacaoDescricao { get; set; } = string.Empty;
+        public string ObservacaoLocal { get; set; } = string.Empty;
+        public string ObservacaoData { get; set; } = string.Empty;
+        public int ObjetoId { get; set; }
+        public int UsuarioId { get; set; }
+
+        public static implicit operator List<object>(ObservacoesModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
